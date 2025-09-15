c9aefd19d64b430277fec0a06621881737845790
mayank123hangsh00 <153893052+mayank123hangsh00@users.noreply.github.com>
2025-09-16 01:41:29 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,15 @@
-variable "region" { default = "ap-south-1" }
-variable "aws_account_id" { type = string }
-variable "image_tag" { default = "latest" }
-variable "service_name" { default = "devops-sample-app" }
-variable "vpc_id" { type = string }
-variable "subnet_ids" { type = list(string) }
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
+variable "image_tag" {
+  default = "latest"
+}
+
+variable "service_name" {
+  default = "devops-sample-app"
+}
