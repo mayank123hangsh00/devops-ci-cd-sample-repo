4ee4a8b5d84553a99977b217a28c028a0a12c5ea
mayank123hangsh00 <153893052+mayank123hangsh00@users.noreply.github.com>
2025-09-17 01:15:10 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "ecr_repo_url" {
-  value = local.ecr_repo_url
+  value = aws_ecr_repository.this.repository_url
 }
 
 output "ecs_cluster_name" {
@@ -11,6 +11,11 @@ output "ecs_service_name" {
 }
 
 output "alb_dns_name" {
-  description = "Application Load Balancer DNS"
-  value       = local.alb_dns_name
+  description = "Application Load Balancer DNS (null if using existing)"
+  value       = try(aws_lb.app_lb[0].dns_name, null)
+}
+
+output "alb_target_group_arn" {
+  description = "Target Group ARN (null if using existing)"
+  value       = try(aws_lb_target_group.app_tg[0].arn, null)
 }
