ebb9ba3c0505811a2624358473ca0b07fe8005b8
mayank123hangsh00 <153893052+mayank123hangsh00@users.noreply.github.com>
2025-09-16 14:15:45 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,16 @@
-output "ecr_repo_url" { value = aws_ecr_repository.this.repository_url }
-output "ecs_cluster_name" { value = aws_ecs_cluster.this.name }
-output "ecs_service_name" { value = aws_ecs_service.this.name }
+output "ecr_repo_url" {
+  value = aws_ecr_repository.this.repository_url
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.this.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.this.name
+}
+
+output "alb_dns_name" {
+  description = "Application Load Balancer DNS"
+  value       = aws_lb.app_lb.dns_name
+}
