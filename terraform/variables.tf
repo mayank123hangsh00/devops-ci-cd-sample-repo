9335e8f1503552d3805fbe5b9a9d345255443ba7
mayank123hangsh00 <153893052+mayank123hangsh00@users.noreply.github.com>
2025-09-16 14:17:18 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,8 +13,15 @@ variable "image_tag" {
 variable "service_name" {
   default = "devops-sample-app"
 }
-variable "vpc_id" { 
-type = string }
 
-variable "subnet_ids" { 
-type = list(string) }
+variable "vpc_id" {
+  default = "vpc-0d117a5cf094c9777"
+}
+
+variable "subnet_ids" {
+  default = [
+    "subnet-0966bab78e8556aac",
+    "subnet-0bbbc05e87102f723",
+    "subnet-02d79f61af69e8c25"
+  ]
+}
