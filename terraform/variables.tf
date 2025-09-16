8cadcde5e6d9ace2aebea75a74901988e5fc929e
mayank123hangsh00 <153893052+mayank123hangsh00@users.noreply.github.com>
2025-09-16 15:27:39 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,13 +15,9 @@ variable "service_name" {
 }
 
 variable "vpc_id" {
-  default = "vpc-0d117a5cf094c9777"
+  type = string
 }
 
 variable "subnet_ids" {
-  default = [
-    "subnet-0966bab78e8556aac",
-    "subnet-0bbbc05e87102f723",
-    "subnet-02d79f61af69e8c25"
-  ]
+  type = list(string)
 }
