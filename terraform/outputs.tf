6c635ee9692ae99f0bd6b3098c2b7d3273a8169d
mayank123hangsh00 <153893052+mayank123hangsh00@users.noreply.github.com>
2025-09-18 23:35:24 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,21 +1,22 @@
 output "ecs_cluster_name" {
-  value = aws_ecs_cluster.app_cluster.name
+  value = aws_ecs_cluster.app.name
 }
 
 output "alb_dns_name" {
   description = "The DNS name of the Application Load Balancer"
-  value       = aws_lb.app_alb.dns_name
+  value       = aws_lb.app.dns_name
 }
 
 output "alb_url" {
   description = "Full URL to access the application via ALB"
-  value       = "http://${aws_lb.app_alb.dns_name}"
+  value       = "http://${aws_lb.app.dns_name}"
 }
 
 output "alb_target_group_arn" {
   description = "Target Group ARN"
-  value       = aws_lb_target_group.app_tg.arn
+  value       = aws_lb_target_group.app.arn
 }
+
 
 
 
